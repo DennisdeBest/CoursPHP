6d40f849cfff3f363a1985ed5337be2ae335ec16
Dennis de Best <Dennis@debest.fr>
2016-02-09 17:08:40 +0100

    Using slim pdo

--- slimTest3/index.php
+++ slimTest3/index.php
@@ -2,32 +2,41 @@
 
 require_once 'vendor/autoload.php';
 $app = new \Slim\App;
+$pdo = new \Slim\PDO\Database('mysql:host=localhost;dbname=CoursPHP;', 'root', '11Dennis');
+
 
 $app->get('/', function ($request, $response, $args) {
     require_once("DB.php");
-    $stt = DB::getInstance()->prepare('SELECT * FROM `jeu_video`');
-    $stt->execute();
-    $temp ="";
-    while ($row = $stt->fetch()) {
-        //$data[] = $row;
-        $nom = $row['nom'];
-        $temp .= "<a href='$nom'>".$row["nom"].PHP_EOL."</a><br />";
+    //$stt = DB::getInstance()->prepare('SELECT * FROM `jeu_video`');
+    //$stt->execute();
+    //$temp ="";
+    global $pdo;
+    $stt = $pdo->select()->from('jeu_video')->execute();
+    $rows = array('<table>');
+    $rows[] = '<tr><th>ID</th><th>Nom</th></tr>';
+    while ($row = $stt->fetch(PDO::FETCH_OBJ)) {
+        $nom = $row->nom;
+        $rows[] = '<tr><td>'.$row->ID."</td><td><a href='$nom'>".$row->nom.'</a></td></tr>';
     }
-    return $response->write($temp);
+    $rows[] = '</table>';
+    $input = file_get_contents('pages/liste.tpl');
+    $output = str_replace('{{jeux}}', implode(PHP_EOL, $rows), $input);
+    return $response->write($output);
 });
 $app->get('/{name}', function ($request, $response, $args) {
     require_once("DB.php");
     $nom = $args['name'];
-    $stt = DB::getInstance()->prepare("SELECT * FROM `jeu_video` WHERE nom='$nom'");
-    $stt->execute();
-    $temp ="";
-    while ($row = $stt->fetch()) {
-        $temp .= "Nom : ".$row["nom"].PHP_EOL."<br />".
-            "Possesseur : ".$row["possesseur"].PHP_EOL."<br />".
-            "Console : ".$row["console"].PHP_EOL."<br />".
-            "Commentaire : ".$row["commentaires"].PHP_EOL."<br />";
+    global $pdo;
+    $stt = $pdo->select()->from('jeu_video')->where('nom', '=', $nom)->execute();
+    $row = $stt->fetch(PDO::FETCH_OBJ);
+    $input = file_get_contents('pages/jeu.tpl');
+    $data = array('ID' => $row->ID,
+        'nom' => $row->nom,
+        'console' => $row->console);
+    foreach ($data as $k => $v) {
+        $input = str_replace('{{' . $k . '}}', $v, $input);
     }
-    return $response->write($temp);
+    return $response->write($input);
 });
     $app->run();
 
---
+++ slimTest3/pages/jeu.tpl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>jeu - {{title}}</title>
+</head>
+<body>
+<table>
+    <tr><td>ID</td><td>{{ID}}</td></tr>
+    <tr><td>nom</td><td>{{nom}}</td></tr>
+    <tr><td>console</td><td>{{console}}</td></tr>
+</table>
+</table>
+</body>
+</html>
