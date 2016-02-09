13bd756b75770ca9e5e89711d5a9261e8cfab363
Dennis de Best <Dennis@debest.fr>
2016-02-09 15:06:44 +0100

    html templtae

--- slimTest2/index.php
+++ slimTest2/index.php
@@ -5,8 +5,28 @@ require_once 'vendor/autoload.php';
 $app = new \Slim\App;
 
 // Define app routes
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    return $response->write("Hello " . $args['name']);
+
+$app->get('/', function ($request, $response, $args) {
+    $content = file_get_contents('pages/index.tpl');
+    $data = array('title' => 'Coucou !!',
+        'subtitle' => 'Accueil',
+        'link_url' => 'contact',
+        'link_name' => 'Page contact');
+    foreach ($data as $k => $v){
+        $content = str_replace('{{' . $k . '}}', $v, $content);
+}
+    return $response->write($content);
+});
+$app->get('/contact', function ($request, $response, $args) {
+    $content = file_get_contents('pages/index.tpl');
+    $data = array('title' => 'Contact',
+        'subtitle' => 'Contact',
+        'link_url' => '../slimTest2/',
+        'link_name' => 'Accueil');
+    foreach ($data as $k => $v){
+        $content = str_replace('{{' . $k . '}}', $v, $content);
+    }
+    return $response->write($content);
 });
 
 // Run app
